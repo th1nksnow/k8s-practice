63280ddf11f3edb33c746a2013636074f750f4d0
Marshall <buffallo654@yandex.ru>
2023-10-12 09:31:26 +0300

    Initial commit

---
+++ docker-image/index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from Kubernetes</h1><br>";
+echo "Server IP Address: $ip_server", "<br><p>";
+echo "Helmed by <font color=blue>Thinksnow";
+
+?>
